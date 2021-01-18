48f3b786d6a5a17eb0336d5c64bd8a77272f0f62
Joshua Campos <chiny.jc@gmail.com>
2021-01-18 20:21:13 +0100

    Change function to male only

--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 def get_female_passengers():
     titanic = pd.read_csv('data/titanic_train.csv')
-    titanic_f = titanic[titanic['Sex']=='female']
+    titanic_f = titanic[titanic['Sex']=='male']
     return titanic_f
